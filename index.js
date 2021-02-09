40ef526234ddc46fecc86ef38925e89bba1f3c98
Seb Bains <sebastianbains@gmail.com>
2021-02-09 17:22:07 +0000

    finished styles, final functionality

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const roundNumber = document.querySelector('.roundNum');
 let round = 0;
 let remainingBlocks = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const pointsNumber = document.querySelector('.pointsNum');
+const currentPointsBar = document.querySelector('.currentPoints');
 let points = 80;
 const totalPointsNum = document.querySelector('.totalPointsNum');
 let totalPoints = 0;
 const roundBuffer = document.querySelector('.roundBuffer');
 const roundButton = document.querySelector('.roundButton');
 const roundMessage = document.querySelector('.roundMessage');
+const roundResults = [];
 
 // get available image from API from provided category
 async function getImage(category) {
@@ -59,15 +61,26 @@ function checkGuess(){
     if(endRound) return;
     const guess = this.innerText.toLowerCase();
     const winRound = guess === category? true : false;
+    roundResults.push(winRound);
     if(winRound){
         totalPoints += points;
         totalPointsNum.innerText = totalPoints;
     }
     endRound = true;
     allBlocks.forEach(block => block.classList.add('hide'));
-    let roundText = winRound? 'Congrats! you were right!' : 'Sorry you were wrong :(';
-    let endText = endGame? `You scored ${totalPoints} points!` : ``;
-    roundMessage.innerHTML = `<p>${roundText}</p><p>${endText}</p>`;
+    let roundText = winRound? `<h3>Congrats!</h3><p>You were correct and earned ${points} points!` : `<h3>Sorry :(</h3><p>You were wrong and don't earn any points this round.`;
+    // let endText = endGame? `You scored ${totalPoints} points!` : ``;
+    let endText = '';
+    let roundResultText = '';
+    const humanResults = roundResults.map(result => {
+        return result? `<span class="won">Won</span>` : `<span class="lost">Lost</span>`;
+        });
+    if(endGame){
+        endText = `<h2>Overall Score</h2><p>You scored a total of <span class="endPoints">${totalPoints}</span> points!<p>`;
+        roundResultText = `<h4>Round Results</h4><p class="endRounds">${humanResults.join(' : ')}</p>`;
+    }
+    const fullMessage = `<p>${roundText}</p><p>${endText}</p><p>${roundResultText}</p>`
+    roundMessage.innerHTML = fullMessage;
     const buttonText = endGame? 'Start New Game' : 'Start Next Round';
     roundButton.innerText = buttonText;
     roundBuffer.classList.remove('hide');
@@ -88,6 +101,8 @@ function resetRound(){
     allBlocks.forEach(block => block.classList.remove('hide'));
     points = 80;
     pointsNumber.innerText = points;
+    const barHeight = ( points / 80) * 100;
+    currentPointsBar.style.height = `${barHeight}%`;
     if( round >= 5){
         endGame = true;
     }
@@ -100,7 +115,8 @@ function resetRound(){
 // periodically removes random remaining block
 var removeBlocks = function(){
     if( endRound || remainingBlocks.length === 0){
-        clearInterval(startRound);
+        clearInterval(roundInterval);
+        clearInterval(pointsInterval);
         return;
     }
     const block = getBlock();
@@ -118,17 +134,27 @@ function startRound(){
         // remove blocks
         const block = getBlock();
         block.classList.add('hide');
-        // reduce points
-        points = points - 10;
-        pointsNumber.innerText = points;
     }, 1000);
+    var pointsInterval = setInterval(() => {
+        if( endRound || remainingBlocks.length === 0){
+            clearInterval(pointsInterval);
+            return;
+        }
+        // reduce points
+        points = points - 1;
+        pointsNumber.innerText = points;
+        const barHeight = ( points / 80) * 100;
+        currentPointsBar.style.height = `${barHeight}%`;
+    }, 100);
 }
 
 // show intro slide/ new round
 function newRound(){
     if( endGame){
         totalPoints = 0;
+        totalPointsNum.innerText = 0;
         round = 0;
+        roundResults.length = 0;
         endGame = !endGame;
     }
     round++;
